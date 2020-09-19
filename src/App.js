54441d07adfc8ce94a4323ad1adc1660cc0c5271
Parallels <samuelvillalvalijo@gmail.com>
2020-09-19 17:47:10 -0500

    Calculator API built

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CalculatorPanel from './CalculatorPanel'
+import Calculator from './Calculator'
+import CalculatorApi from './CalculatorApi'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CalculatorPanel />
+        <Calculator calculatorApi={new CalculatorApi('http://127.0.0.1:8080')} />
       </header>
     </div>
   );
